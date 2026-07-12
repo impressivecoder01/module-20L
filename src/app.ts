2e228107563890558809583cbb0bf4e2da919e78
impressivecoder1 <rohanrahman251@gmail.com>
2026-07-13 01:38:05 +0600

    user and profile schema done

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,18 @@
 import express, {  Application, Request, Response } from "express";
+import cookieParser from "cookie-parser"
+import cors from "cors"
+import config from "./config";
 
-const app: Application = express()
+const app: Application = express();
+
+app.use(cors({
+    origin: config.app_url,
+    credentials: true
+}))
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(cookieParser())
 
 app.get("/",(req: Request,res: Response)=>  {
     res.send("Hello world")
