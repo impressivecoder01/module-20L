233c75e12d991ae7fb0b1ae551a82daac2d7a8e6
impressivecoder1 <rohanrahman251@gmail.com>
2026-07-17 00:34:39 +0600

    sendResponse function  created

--- src/modules/users/user.controller.ts
+++ src/modules/users/user.controller.ts
@@ -5,6 +5,7 @@ import httpStatus from "http-status";
 import { NextFunction, Request, RequestHandler, Response} from "express";
 import { userService } from "./user.service";
 import catchAsync from "../../utils/catchAsync";
+import { sendResponse } from "../../utils/sendResponse";
 
 
 
@@ -29,16 +30,24 @@ import catchAsync from "../../utils/catchAsync";
     // }
 
 // }
+
+
 const registerUser = catchAsync(async(req: Request, res: Response, next: NextFunction)=> {
     const payload = req.body;
     const user = await userService.registerUserIntoDB(payload)
-    res.status(httpStatus.CREATED).json({
+    // res.status(httpStatus.CREATED).json({
+    //     success: true,
+    //     statusCode: httpStatus.CREATED,
+    //     message: "User registered successfully",
+    //     data: {
+    //         user
+    //     }
+    // })
+    sendResponse(res, {
         success: true,
         statusCode: httpStatus.CREATED,
         message: "User registered successfully",
-        data: {
-            user
-        }
+        data: {user}
     })
 })
 
