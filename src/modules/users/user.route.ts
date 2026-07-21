dce47a81eff022d6cd220edee976e34ac7c60a3f
impressivecoder1 <rohanrahman251@gmail.com>
2026-07-22 01:53:45 +0600

    auth middleware ready

--- src/modules/users/user.route.ts
+++ src/modules/users/user.route.ts
@@ -6,6 +6,7 @@ import { Role } from "../../../generated/prisma/enums";
 import httpStatus from "http-status";
 import catchAsync from "../../utils/catchAsync";
 import { JwtPayload } from "jsonwebtoken";
+import { prisma } from "../../lib/prisma";
 
 
 
@@ -26,13 +27,50 @@ declare global {
 
 router.post("/register",userController.registerUser)
 
-const auth = () => {
+const auth = (...requiredRoles: Role[]) => {
     return catchAsync(async(req: Request, res: Response, next: NextFunction)=> {
         const token = req.cookies.accessToken || (req.headers.authorization?.startsWith("Bearer")? req.headers.authorization?.split(" ")[1]: req.headers.authorization)
         if(!token){
             throw new Error("you are not logged in. Please log in to access this resource.")
         }
-        const verifiedToken = jwtUtils.verifyToken(token, config.jwt_access_secret)
+        const verifiedToken = jwtUtils.verifyToken(token, config.jwt_access_secret);
+
+    
+    // if(typeof verifiedToken === 'string'){
+    //     throw new Error(verifiedToken)
+    // }
+    if(!verifiedToken.success){
+        throw new Error(verifiedToken.error)
+    }
+        const {email, name, id, role} = verifiedToken.data as JwtPayload
+        if(requiredRoles.length && !requiredRoles.includes(role)){
+            throw new Error("Forbidden. You do not have permission to  access this resource")
+        }
+
+        const user = await prisma.user.findUnique({
+            where: {
+                id, 
+                email,
+                name,
+                role
+            }
+        })
+
+        if(!user){
+            throw new Error("User not found. Please log in again")
+        }
+
+        if(user.activeStatus==="BLOCKED"){
+            throw new Error ("Your account has been blocked. Please contact  support")
+        }
+
+        req.user={
+            email,
+            name,
+            id,
+            role
+        }
+
     })
 }
 
