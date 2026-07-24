45567fd5e5485ccdeb70b331c67406b5864b8aca
impressivecoder1 <rohanrahman251@gmail.com>
2026-07-25 00:43:22 +0600

    manage comment route

--- src/modules/comments/comment.route.ts
+++ src/modules/comments/comment.route.ts
@@ -1,5 +1,19 @@
 import { Router } from "express";
+import { auth } from "../../auth/auth";
+import { Role } from "../../../generated/prisma/enums";
+import { commentController } from "./comment.controller";
 
-const router = Router
+const router = Router()
 
+router.post("/",auth(Role.ADMIN, Role.AUTHOR, Role.USER), commentController.createComment)
+
+router.get("/author/:authorId", commentController.getCommentByAuthorId)
+
+router.get("/:commentId",  commentController.getCommentByCommentId)
+
+router.patch("/:commentId", auth(Role.ADMIN, Role.AUTHOR, Role.USER), commentController.updateComment)
+
+router.delete("/:commentId", auth(Role.ADMIN, Role.AUTHOR, Role.USER), commentController.deleteComment)
+
+router.put("/:commentId/moderate", auth(Role.ADMIN, Role.AUTHOR, Role.USER), commentController.moderateComment)
 export const commentRouters  = router;
---
+++ src/modules/posts/post.service.ts
@@ -0,0 +1,19 @@
+
+const createPost =()=>{}
+const getAllPosts =()=>{}
+const getPostStats =()=>{}
+const getMyPosts =()=>{}
+const getPostById =()=>{}
+const updatePost =()=>{}
+const deletePost =()=>{}
+
+export const postService = {
+    deletePost,
+    updatePost,
+    getPostById,
+    getMyPosts,
+    getPostStats,
+    getAllPosts,
+    createPost
+
+}
